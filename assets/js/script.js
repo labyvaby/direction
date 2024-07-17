3cb8f55c429ac96338a80904c8439b3897da55b1
laby <dipt688@gmail.com>
2024-07-17 10:16:26 +0300

    add all prfoile

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const items = document.querySelectorAll(".header__item");
+
+//     items.forEach(item => {
+//         const tooltipText = item.getAttribute("data-tooltip");
+//         if (tooltipText) {
+//             const tooltipElement = document.createElement("div");
+//             tooltipElement.classList.add("tooltip");
+//             tooltipElement.innerHTML = tooltipText;
+//             item.appendChild(tooltipElement);
+//         }
+//     });
+// });
